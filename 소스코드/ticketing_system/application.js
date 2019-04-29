f78bdd747cd52ea91ad05b048f4da32306692450
yhye97 <yhye1997@gmail.com>
2019-04-29 13:59:37 +0900

    source code

---
+++ 소스코드/ticketing_system/application.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mysql      = require('mysql');
+var request = require('request');
+var index = require('./routes/index');
+var buyers = require('./routes/buyers');
+var bidding = require('./routes/bidding');
+var administrators =require('./routes/administrators');
+var organizers =require('./routes/organizers');
+var gigs =require('./routes/gigs');
+var seats =require('./routes/seats');
+var sections =require('./routes/sections');
+
+var passport = require('passport');
+var setting = require('./routes/setting');
+var session = require('express-session');
+
+//port
+passport.serializeUser(function(buyers, done) {
+    console.log('serialized');
+    done(null, buyers);
+});
+passport.deserializeUser(function(buyers, done) {
+    console.log('deserialized');
+    done(null, buyers);
+});
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+//mount path
+app.get('/', function (req, res) {
+    console.log(app.mountpath);
+    res.send('App Homepage');
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    port     : 3306,
+    database : 'ticketing_service'
+});
+
+app.use(session({
+    secret: 'secrettexthere',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', index);
+app.use('/buyers', buyers);
+app.use('/administrators', administrators);
+app.use('/organizers', organizers);
+app.use('/gigs', gigs);
+app.use('/sections', sections);
+app.use('/seats', seats);
+app.use('/bidding', bidding);
+app.use('/setting', setting);
+app.use(express.static('views'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+var server = app.listen(3000);
+module.exports = app;
+console.log("hi");
---
+++ 소스코드/ticketing_system/routes/index.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+var mysql      = require('mysql');
+var passport = require('passport'),
+    KakaoStrategy = require('passport-kakao').Strategy;
+
+passport.use(new KakaoStrategy({
+        clientID : '5d61e3764a3ee0f12ccace0e1fc18bb5',
+        callbackURL :'/auth/login/kakao/callback',
+      //clientSecret : 'eUtJGtlLoCZJufevp3LKfDP0KOtZUV7R'
+    },
+    function(accessToken, refreshToken,params, profile, done){
+        //사용자 정보는 profile에
+        loginByThirdparty(accessToken, refreshToken, profile);
+
+        console.log("(!)로그인 : " + profile._json.id+"("+profile._json.properties.nickname +")");
+        //return done(null,profile)
+        return done(null, {
+            'user_id': profile._json.id,
+            'nickname': profile._json.properties.nickname
+        });
+    }
+));
+
+// kakao 로그인
+router.get('/auth/login/kakao',
+    passport.authenticate('kakao')
+);
+
+// kakao 로그인 연동 콜백
+router.get('/auth/login/kakao/callback',
+    passport.authenticate('kakao', {
+        //session: false,
+        successRedirect: '/mypage',
+        failureRedirect: '/'
+    })
+);
+
+function loginByThirdparty(accessToken, refreshToken, profile) {
+    //예전 코드는 MySQL 버젼이 맞지 않음
+    //  var sql = 'INSERT INTO `user`(id) VALUES(?) ON DUPLICATE KEY(PRIMARY) UPDATE id=(?);'
+    var sql = "INSERT INTO `user` (id) VALUES (?) ON DUPLICATE KEY UPDATE id=id";
+    var kid=[profile._json.id];
+    connection.query(sql,kid,function(err,result){
+        if (err) {
+            console.log("로그인 쿼리중 에러 : " + err);
+        } else {
+            console.log("로그인 DB처리 완료!");
+        }
+    });
+}
+
+router.get('/auth/logout/kakao',function (req,res) {
+    req.logout();
+    res.redirect('/');
+})
+
+/* GET home page. */
+router.get('/',
+    function(req,res,next){
+        if(req.isAuthenticated()){
+            res.redirect('/mypage');
+            console.log("(!)이미 로그인");
+        }else{
+            console.log("(!)로그인세션 없음");
+
+        }
+    });
+
+module.exports = router;
