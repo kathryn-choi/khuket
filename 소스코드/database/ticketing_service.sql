bdefa7a08d7088054d4b83c199cd2e47670fd59f
김기빈 <kbkim@khu.ac.kr>
2019-05-22 08:07:40 +0900

    Modified .sql

--- 소스코드/database/ticketing_service.sql
+++ 소스코드/database/ticketing_service.sql
@@ -1,13 +1,15 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `ticketing_service` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ticketing_service`;
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: ticketing_service
+-- Host: localhost    Database: ticketing_service
 -- ------------------------------------------------------
--- Server version	5.6.44
+-- Server version	5.7.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +23,7 @@
 
 DROP TABLE IF EXISTS `administrators`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `administrators` (
   `admin_index` int(11) NOT NULL AUTO_INCREMENT,
   `admin_id` varchar(45) NOT NULL,
@@ -34,7 +36,7 @@ CREATE TABLE `administrators` (
   `updatedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`admin_index`),
   UNIQUE KEY `admin_id_UNIQUE` (`admin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,6 +45,7 @@ CREATE TABLE `administrators` (
 
 LOCK TABLES `administrators` WRITE;
 /*!40000 ALTER TABLE `administrators` DISABLE KEYS */;
+INSERT INTO `administrators` VALUES (1,'admin','ba16146b49dcddfca06f5d074eb703b6cc4ac23386e647c6de2fc546d7fb9d0860212f9a152e778fa3fedf7d364b83a2dc70c76d2c88aff9c8f5294acc7a323f','admin@admin.com',1012341234,'administrator','336537240567','2019-05-21 22:56:18','2019-05-21 22:56:18');
 /*!40000 ALTER TABLE `administrators` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,7 +55,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `biddings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `biddings` (
   `bidding_index` int(11) NOT NULL AUTO_INCREMENT,
   `current_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -85,7 +88,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `buyers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `buyers` (
   `buyer_index` int(11) NOT NULL AUTO_INCREMENT,
   `buyer_id` varchar(100) NOT NULL,
@@ -102,7 +105,7 @@ CREATE TABLE `buyers` (
   UNIQUE KEY `b_id_UNIQUE` (`buyer_id`),
   UNIQUE KEY `b_index_UNIQUE` (`buyer_index`),
   UNIQUE KEY `b_contact_UNIQUE` (`buyer_contact`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,6 +114,7 @@ CREATE TABLE `buyers` (
 
 LOCK TABLES `buyers` WRITE;
 /*!40000 ALTER TABLE `buyers` DISABLE KEYS */;
+INSERT INTO `buyers` VALUES (1,'buyer','e70ac409c6431a4f9af5beefa5a13c58179226fa31817d8b305815a990455595c7098d9687c390b171a80514c779567bccd4ba9b0de86ebdcc547a831ba8635f','buyer@buyer.com',1012341234,1234234243,'buyer',NULL,'1182351900085','2019-05-21 22:56:57','2019-05-21 22:56:57');
 /*!40000 ALTER TABLE `buyers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +124,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `gigs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `gigs` (
   `gig_index` int(11) NOT NULL AUTO_INCREMENT,
   `gig_organizer_index` int(11) NOT NULL,
@@ -155,11 +159,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `notifications`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `notifications` (
   `notification_index` int(11) NOT NULL AUTO_INCREMENT,
   `notice_buyer_id` varchar(45) NOT NULL,
   `notice_buyer_text` varchar(300) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`notification_index`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -170,7 +176,7 @@ CREATE TABLE `notifications` (
 
 LOCK TABLES `notifications` WRITE;
 /*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
-INSERT INTO `notifications` VALUES (1,'1078842445','notice1'),(3,'-1','overbid ??????!'),(4,'1078842445','you have been overbid!');
+INSERT INTO `notifications` VALUES (1,'1078842445','notice1',NULL,NULL);
 /*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -180,19 +186,21 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `organizers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `organizers` (
   `organizer_index` int(11) NOT NULL AUTO_INCREMENT,
   `organizer_id` varchar(45) NOT NULL,
-  `organizer_pw` varchar(45) NOT NULL,
+  `organizer_pw` varchar(300) NOT NULL,
   `organizer_email` varchar(45) DEFAULT NULL,
   `organizer_contact` int(11) DEFAULT NULL,
-  `organizer_account` int(11) DEFAULT NULL,
+  `organizer_account` bigint(20) DEFAULT NULL,
   `organizer_name` varchar(45) DEFAULT NULL,
   `salt` varchar(100) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`organizer_index`),
   UNIQUE KEY `organizer_id_UNIQUE` (`organizer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -201,7 +209,7 @@ CREATE TABLE `organizers` (
 
 LOCK TABLES `organizers` WRITE;
 /*!40000 ALTER TABLE `organizers` DISABLE KEYS */;
-INSERT INTO `organizers` VALUES (1,'og1','pw','og1@og.com',123123,11111,'organizer1',NULL);
+INSERT INTO `organizers` VALUES (2,'organizer','d4d92bf035bbe15e88f6d3345ac0fccb870ee89aded6469f0b24880fc42ea5d5d90a8b162d1027855c040f5e12ca77ed15bf476521de63a7b8e7e2ac7618b350','organizer@organizer.com',1043214321,2342431234,'organizer','235808721963','2019-05-21 22:58:15','2019-05-21 22:58:15');
 /*!40000 ALTER TABLE `organizers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -211,7 +219,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `seats`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `seats` (
   `seat_index` int(11) NOT NULL AUTO_INCREMENT,
   `gig_index` int(11) NOT NULL,
@@ -244,7 +252,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `sections`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `sections` (
   `section_index` int(11) NOT NULL AUTO_INCREMENT,
   `gig_index` int(11) NOT NULL,
@@ -276,4 +284,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-19 18:59:27
+-- Dump completed on 2019-05-22  7:59:15
