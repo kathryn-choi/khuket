8161180680fa711677202617338bd7f8b7f1002c
kathryn-choi <mody135@gmail.com>
2019-06-05 00:19:17 +0900

    Admin page auto refresh added

--- 소스코드/routes/administrators.js
+++ 소스코드/routes/administrators.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const models = require("../models");
 const crypto = require("crypto");
+const util = require('util');
+var async = require('async');
 var network = require('../ticketing-system/network.js');
 
 router.get('/login', function(req, res, next) {
@@ -52,142 +54,158 @@ router.get('/', function(req, res, next) {
     });
 });
 
-function create_ticket(gig_index, cb){
+async function create_ticket(gig_index, cb){
     var sqlquery = "SELECT  * FROM gigs  WHERE gig_index=?";
     var gig_ticket=new Array();
 
     var result=""
+    const query = util.promisify(connection.query).bind(connection);
+    var rows = await query(sqlquery,gig_index)
+    await console.log("1")
+    await console.log(rows)
+    if(rows.length != 0){
+        gig_ticket=rows;
+        var sqlquery2 = "SELECT  * FROM sections  WHERE gig_index=?";
+        var sections=new Array();
+        var _gig_index = gig_ticket[0].gig_index;
+        var gig_date_time = (gig_ticket[0].gig_date_time).toString();
+        var gig_name = gig_ticket[0].gig_name;
+        var gig_venue = gig_ticket[0].gig_venue;
+        var total_seat_num = gig_ticket[0].gig_total_seatnum;;
+        var row2 = await query(sqlquery2,gig_index)
+        await console.log("2");
+            if(row2.length != 0) {
+                await console.log(1);
+                await console.log(row2)
+                sections=row2;
+                await console.log("sections:",sections)
+                var count = 0
+                
+                for (var i=0; i<sections.length; i++)
+                {
+                    await console.log(2);
+                    var section_id=sections[i].section_id;
+                    var section_index=sections[i].section_index;
+                    var seat_price = sections[i].seat_price
+                    var sqlquery3 = "SELECT * FROM seats  WHERE section_id=? AND gig_index=?";
+                    var seats=new Array();
+                    var values=[section_id, _gig_index];
+                    var row3 = await query(sqlquery3, values)
+                    if(row3.length !=0){
+                        await console.log(3);
+                        seats=row3;
+                        await console.log(row3);
+                        for (var j=0; j<seats.length; j++)
+                        {
+                            await console.log(4);
 
-    connection.query(sqlquery,gig_index,function(err,rows){
-        console.log("1")
-        if(!err){
-            gig_ticket=rows;
-            var sqlquery2 = "SELECT  * FROM sections  WHERE gig_index=?";
-            var sections=new Array();
-            var _gig_index = gig_ticket[0].gig_index;
-            var gig_date_time = (gig_ticket[0].gig_date_time).toString();
-            var gig_name = gig_ticket[0].gig_name;
-            var gig_venue = gig_ticket[0].gig_venue;
-            var total_seat_num = gig_ticket[0].gig_total_seatnum;;
-            connection.query(sqlquery2,gig_index,function(err,rows){
-                console.log("2");
-                if(!err) {
-                    console.log(1);
-                    sections=rows;
-                    console.log("sections:",sections)
-                    var count = 0
-                    
-                    for (var i=0; i<sections.length; i++)
-                    {
-                        console.log(2);
-                        var section_id=sections[i].section_id;
-                        var section_index=sections[i].section_index;
-                        var seat_price = sections[i].seat_price
-                        var sqlquery3 = "SELECT * FROM seats  WHERE section_id=? AND gig_index=?";
-                        var seats=new Array();
-                        var values=[section_id, _gig_index];
-                        connection.query(sqlquery3, values, function(err,rows) {
-                        if(!err){
-                            console.log(3);
-                            seats=rows;
-                            console.log(rows);
-                            for (var j=0; j<seats.length; j++)
-                            {
-                                console.log(4);
-
-                                var section_id = seats[j].section_id;
-                                var seat_index=seats[j].seat_row_index;
-                                var ticket_id=(_gig_index).toString() + "." +section_id +"." +(seat_index).toString();
-                                var seat_row_index=seats[j].seat_row_index;
-                                console.log("ticket_id" ,ticket_id);
-                                network.create_ticket('ticketadmin',ticket_id,section_id,seat_row_index,seat_index,seat_price,_gig_index,gig_date_time, gig_name, gig_venue)  
-                                .then((response) => {
-                                    //return error if error in response
-                                    if (response.error != null) {
-                                        console.log("network create_ticket failed");
-                                        throw err;
-                                    } else {
-                                      //else return success
-                                        console.log("return true complete")
-                                        count++
-                                        result = true
-                                        //cb(result);
-                                    }
-                                  })
-                                .then((response) =>{
-                                    if (count == total_seat_num){cb(result);};
+                            var section_id = seats[j].section_id;
+                            var seat_index=seats[j].seat_row_index;
+                            var ticket_id=(_gig_index).toString() + "." +section_id +"." +(seat_index).toString();
+                            var seat_row_index=seats[j].seat_row_index;
+                            await console.log("ticket_id" ,ticket_id);
+                            await network.create_ticket('ticketadmin',ticket_id,section_id,seat_row_index,seat_index,seat_price,_gig_index,gig_date_time, gig_name, gig_venue)  
+                            .then((response) => {
+                                //return error if error in response
+                                if (response.error != null) {
+                                    console.log("network create_ticket failed");
+                                    throw err;
+                                } else {
+                                    //else return success
+                                    console.log("return true complete")
+                                    count++
+                                    result = true
+                                    //cb(result);
+                                }
                                 })
-                            }
+                            .then((response) =>{
+                                if (count == total_seat_num){cb(result);};
+                            })
                         }
-                        else{
-                            console.log("selecting seats error")
-                            result= false;
-                        }
-                        });
                     }
-                    console.log(5)
-                    result=true;
-                } else{
-                console.log("gig type list를 가져오는데 실패했습니다!");
-                result=false;
+                    else{
+                        await console.log("selecting seats error")
+                        result= false;
+                    }
+                   
                 }
-                console.log(6)
-            });
-           
-        }else{
-            console.log("gig type list를 가져오는데 실패했습니다!");
-           result =false;
-        }
-        });
-    console.log("create ticket result:",result)
+                await console.log(5)
+                result=true;
+            } else{
+                await console.log("gig type list를 가져오는데 실패했습니다!");
+            result=false;
+            }
+            await console.log(6)
+
+        
+    }else{
+        await console.log("gig type list를 가져오는데 실패했습니다!");
+        result =false;
+    }
+    await  console.log("create ticket result:",result)
     cb(result);
 }
 
-function create_seats(cb){
-    
-}
-
 // pending = 2인 공연들 목록들 출력하고 승인 누르면 pending = 1로 변경되고 티켓 생성
-router.post('/accept_gig', function(req, res, next) {
+router.post('/accept_gig', async (req, res, next) => {
     let session = req.session;
        var gig_index=req.body.gig_index;
        var pending=req.body.pending;
-       console.log("pending : "+pending);
-       if(pending=='accept')
-       {
-           pending=1;
-           create_ticket(gig_index, function(result){
-               if(result==true){
-                   console.log("creating ticket succeed!")
-                   var sql = "UPDATE gigs SET pending = ?  WHERE gig_index = ?";
-                   connection.query(sql, [pending, gig_index], function (err) {
-                       if (err) {
-                           console.log("updating failed");
-                            throw err;
-                       } else {
-                           console.log("gig pending update");
-                       }
-                   });
-                   res.redirect('/administrators/');
-               }else{
-                       console.log("creating ticket failed")
-                    }
-                });
-                res.redirect('back');
-       }
-       else {
-           pending=0;
-           var sql = "UPDATE gigs SET pending = ?  WHERE gig_index = ?";
-           connection.query(sql, [pending, gig_index], function (err) {
-               if (err) {
-                   console.log("updating failed");
-                   throw err;
-               }
-               else {
-                   console.log("gig pending update");
-               }
-           });
-           res.redirect('back');
-       }
+       var temp = true;
+       await console.log("pending : "+pending);
+       if(pending == 'accept')
+        {
+            pending=1;
+            
+            await console.log("I'm here")
+            await create_ticket(gig_index, function(result){
+                if(result==true){
+                    console.log("creating ticket succeed!")
+                    var sql = "UPDATE gigs SET pending = ?  WHERE gig_index = ?";
+                    connection.query(sql, [pending, gig_index], function (err) {
+                        if(!err) {
+                            console.log("gig pending update");
+                                temp = true
+                        } else {
+                            console.log("updating failed");
+                                temp = false
+                        }
+                    });
+                }else{
+                    console.log("creating ticket failed")
+                        temp = false
+                    
+                    return
+                }
+            });
+
+            if(temp == true){
+                await res.jsonp({success : true})
+            }
+            else{
+                await res.jsonp({success : false})
+            }
+            
+        }          
+        else {
+            pending=0;
+            var sql = "UPDATE gigs SET pending = ?  WHERE gig_index = ?";
+            connection.query(sql, [pending, gig_index], function (err) {
+                if (err) {
+                    console.log("updating failed");
+                    temp = false
+                }
+                else {
+                    console.log("gig pending update");
+                    temp = true
+                }
+            });
+            if(temp == true){
+                await res.jsonp({success : true})
+            }
+            else{
+                await res.jsonp({success : false})
+            }
+        }     
 });
 module.exports = router;
