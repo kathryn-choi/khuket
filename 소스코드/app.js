e7617d78765d9ee323723cdebae523e8cee670d2
kbkim <kbkim@khu.ac.kr>
2019-06-04 23:31:41 +0900

    Modified reselling page

--- 소스코드/app.js
+++ 소스코드/app.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'pw',
+    password : 'password123',
     port     : 3306,
     database : 'ticketing_service',
 });
